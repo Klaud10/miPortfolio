7d4e8bf9d704545e82d84af5b8975877731c163f
[Klaud10] <[claudio.falabella@gmail.com]>
2021-11-05 11:18:54 +0000

    5-11-2021

---
+++ script.js
@@ -0,0 +1,19 @@
+(function(){
+
+    // selector
+var menu = document.querySelector('.navegacion__movil');
+
+// method
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
+
+
+})();
+
+
